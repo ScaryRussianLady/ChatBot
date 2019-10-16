785a64132c05e3cf0ec99d9f4171a145d28116a3
Annija Balode <35531855+ScaryRussianLady@users.noreply.github.com>
2019-10-17 00:05:16 +0100

    Retrieves top 10 news of today on NewsAPI.py
    https://code.visualstudio.com/docs/python/environments

--- NewsAPI.py
+++ NewsAPI.py
@@ -4,7 +4,7 @@ import requests
 def NewsFromBBC(): 
 	
 	# BBC news api 
-	main_url = " https://newsapi.org/v1/articles?source=bbc-news&sortBy=top&apiKey=4dbc17e007ab436fb66416009dfb59a8"
+	main_url = " https://newsapi.org/v1/articles?source=bbc-news&sortBy=top&apiKey=72742ae51f514418a9a6da52faf58be6"
 
 	# fetching data in json format 
 	open_bbc_page = requests.get(main_url).json() 
