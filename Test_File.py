07e43186787884d455bb0d6699c7ff8957f318b2
RishikeshS13 <56678998+RishikeshS13@users.noreply.github.com>
2019-10-17 16:19:45 +0100

    Pal Print
    line 5 of the code

--- Test_File.py
+++ Test_File.py
@@ -1,5 +1,5 @@
 #type in print("hello 'your name'") replace 'your name' with your actual name please.
 print("Hello Jamie")
 print("Test")
-print("Hello Callum is a legend")
-print("Hello Kesh")
+print("Hello Callum is a puppet")
+print("Hello Kesh pal")
