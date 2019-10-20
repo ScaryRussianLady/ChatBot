38fbf73ac62c6066e5bd5cf9e815f243342bcc3d
Annija Balode <35531855+ScaryRussianLady@users.noreply.github.com>
2019-10-20 12:17:29 +0100

    Cleaned up the NewsAPI.py.
    Added necessary comments to make it clear what is occuring. Will expand on this file later on.

--- NewsAPI.py
+++ NewsAPI.py
@@ -1,33 +1,29 @@
-# importing requests package 
+#This imports the requests package which allows for HTTP requests.
 import requests	 
 
-
-#I will comment the entire script for you by tonight christian, so you understand what is happening within this entire file at the moment.
+#Function for fetching the top ten headlines of today.#
+#[Annija] and referenced from https://www.geeksforgeeks.org/fetching-top-news-using-news-api/#
 def NewsFromBBC(): 
-	
-	# BBC news api 
+	#This is the BBC News API with our own personal API key. 
 	main_url = " https://newsapi.org/v1/articles?source=bbc-news&sortBy=top&apiKey=72742ae51f514418a9a6da52faf58be6"
 
-	# fetching data in json format 
+	#Fetches the data in JSON format. 
 	open_bbc_page = requests.get(main_url).json() 
 
 	# getting all articles in a string article 
 	article = open_bbc_page["articles"] 
 
-	# empty list which will 
-	# contain all trending news 
+	#This is an empty list which will contain all of the results (the articles).
 	results = [] 
 	
+	#Adds the title of the article to the results list.
 	for ar in article: 
 		results.append(ar["title"]) 
-		
+	
 	for i in range(len(results)): 
-		
-		# printing all trending news 
+		#Simply prints all the trending news right now (top ten).
 		print(i + 1, results[i])				 
 
-# Driver Code 
-if __name__ == '__main__': 
-	
-	# function call 
+	#Calls the function, only using this to ensure that everything is getting called correctly, these function names will
+	#be used to import from chatbot_script.py later on.
 	NewsFromBBC() 
