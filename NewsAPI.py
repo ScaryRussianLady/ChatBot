1b3ec770006d5b86b080d1ccccbd3937fa63c8df
Annija Balode <35531855+ScaryRussianLady@users.noreply.github.com>
2019-10-20 13:47:52 +0100

    Added a new function in NewsAPI.py
    A new function which allows for specific news to be fetched depending on key words/terms. This will be expanded to adjust depending on what the user says.

--- NewsAPI.py
+++ NewsAPI.py
@@ -15,12 +15,32 @@ def EveryTopHeadline():
 	   'category=general&'
 	   #Returns a set amount of news articles, default will be 20 if you do not specify and the maximum is 100, but we should not go
 	   #above 10 realistically, or however many the user will request for.
-		'pageSize=5&'
+		'pageSize=1&'
        'apiKey=72742ae51f514418a9a6da52faf58be6')
 	response = requests.get(url)
 	
 	#Fetches the data in JSON format.
 	theURL = response.json()
+	print(theURL)
+
+#A function for finding news around the world depending on specific words/key terms.
+#However, this search does not allow for country-specific searches.
+def SpecificNews():
+	url = ('https://newsapi.org/v2/everything?'
+	#This is the key word that should appear in the title of the article.
+       'q=Murder&'
+	   'pageSize=1&'
+	   #From what date it should get searched (depending how old you want the article to be)
+       'from=2019-10-20&'
+	   #How to sort the articles (the most popular will show first, therefore, the most relevant and most likely to be in English)
+       'sortBy=popularity&'
+       'apiKey=72742ae51f514418a9a6da52faf58be6')
+
+	response = requests.get(url)
+
+	#Fetches the data in JSON format.
+	theURL = response.json()
+
 	print(theURL)
 
 #Function for fetching the top ten headlines of today from BBC News.#
@@ -51,3 +71,4 @@ if __name__ == '__main__':
 	#be used to import from chatbot_script.py later on.
 		NewsFromBBC()
 		EveryTopHeadline() 
+		SpecificNews()
