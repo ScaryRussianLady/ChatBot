10e22d7a44119d2c6d594e6bb15376037eb28471
jamdjam <warnock.jamie@yahoo.com>
2019-10-20 22:45:56 +0100

    Update TMDBAPI.py
    will take an input from user and turn it into a search for movie titles.
    presentation of output needs to be worked on/fixed

--- TMDBAPI.py
+++ TMDBAPI.py
@@ -7,7 +7,7 @@ def movie_search():
 
     #this inputs the keyword(s) into the search
     #"shutte island" will only return one result; easy to read for now 
-    query = input("please type in a keyword: ")
+    query = input("please type in the movie title you wish to search for: ")
 
     #takes the input from query and replaces the spaces with "%20"
     #"%20" represents a space in a link
@@ -16,9 +16,11 @@ def movie_search():
     #this implements the api key and sQuery so we do not have to constantly type it out
     url = "https://api.themoviedb.org/3/search/movie/?api_key="+api_key+"&language=en-US&query="+sQuery+"&page=1&include_adult=false"
 
-    #retreives information from the search and prints to the screen as somewhat readable text
+    #retreives information from the search and prints to the screen as somewhat readable text    
     response = req.get(url)
-    movDict = response.text
-    print(movDict)
+    movDict = response.json()
+    print(list(movDict["results"]))
+    #print(type(movDict))
+    #print("page" in movDict)
 
 movie_search()
