06b85d4f092bc439ffc4403f4209bc7146671a3b
Annija Balode <35531855+ScaryRussianLady@users.noreply.github.com>
2019-10-23 19:36:56 +0100

    Key and Secrey set up in GoodreadsAPI.py just in case

---
+++ GoodreadsAPI.py
@@ -0,0 +1,5 @@
+#The key: JnUP42tF3SjMYKotdkgrSA#
+#The secret: yy1EpTPtnKAUZLljIkeSZ8BJ9cc0QB70K6GeGQNGB0#
+url = open("https://www.goodreads.com/search.xml?key=JnUP42tF3SjMYKotdkgrSA&q=Ender%27s+Game")
+
+	
