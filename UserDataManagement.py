7caa8e5619f187486f4036f2801b323b0b8c7bf3
Christian <I-Dont-Dev@users.noreply.github.com>
2019-10-24 19:47:05 +0100

    Created a script that will handle userdata with instructions

---
+++ UserDataManagement.py
@@ -0,0 +1,15 @@
+# Use this script to manage the User_Datastore.json file
+
+# Create a function which takes an object as it's variable
+# This are the methods is inside of the object
+  
+#   ("User's message            >>", msgObj.msg)
+#   ("User's message as list    >>", msgObj.list)
+#   ("User's message's language >>", msgObj.lang)
+#   ("User's ID                 >>", msgObj.userID)
+
+# Use the object accordingly in order to manage the User_Datastore.json
+
+import json
+
+
