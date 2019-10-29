726a6721cb3d62924420485bfc3641ed56d734e6
Callum <Callum.jones5@btinternet.com>
2019-10-29 21:32:27 +0000

    Adding to Lists in Dictionary Now Works
    can now add to a list of favourite genres within the dictionary under the userid and dump to json file

--- UserDataManagement.py
+++ UserDataManagement.py
@@ -4,26 +4,27 @@ import json
 
 # [CALLUM] This code allows the program to update the jason file with users previous responses and allows for multiple users 
 def SaveData(Data, UserID, Location):
+
+    count = 0
+    
     with open("User_Datastore.json") as uds:
         UserData = json.load(uds)
-    catagories = ["LastMessage", "LastFilmReply" , "LastNewsReply","LastBookReply"] #List of items taht can only take a single argument
-    count = 0
+    
     if IsNewID(UserData, UserID):
         UserData = CreateNewID(UserData, UserID)
     
-
     for entry in UserData['data']:  # This code check through the entrys of User Data to find the correct User ID entry
         if entry['UserID'] == UserID:
-            for Cat in catagories:
-                if Location == Cat:
-                    UserData['data'][count][Location] = Data # This replaces the entry with the updated user data
-                else:
-                    UserData['data'][count][Location].append(Data) # This adds to a list for the gneres to be saved
+            if Location == "Name" or Location == "LastMessage" or Location ==  "LastFilmReply" or Location ==  "LastNewsReply" or Location == "LastBookReply": #these catagories can only take a single argument
+                UserData['data'][count][Location] = Data # This replaces the entry with the updated user data
+            else:
+                UserData['data'][count][Location].append(Data) # This adds to a list to add to user data
         count += 1
+    
     with open("User_Datastore.json", 'w') as uds: #writes updated data to json file
         json.dump(UserData, uds , indent= 3)
 
-    # Here will save the data to that specific ID
+   
 
 # [CHRISTIAN] Checks if UserID already exists in the datastore and returns false if it does and true if it doesn't
 # [CALLUM] changed the loop to incorporate new method of checking through the file, old loop commented out below if neeeded
@@ -49,9 +50,9 @@ def CreateNewID(UserData, UserID):
     "FavFilmGenre": ["Example Genre", "Another Example"],
     "FavBookGenre": ["Example Genre", "Another Example"],
     "FavNewsTopic": ["Example Topic", "Another Example"],
-    "Previous Viewed Films": ["Example Film", "Another Example"], 
-    "Previous Viewed Books": ["Example Book", "Another Example"],
-    "Previous Viewed Articles": ["Example Article", "Another Example"],})
+    "PreviousViewedFilms": ["Example Film", "Another Example"], 
+    "PreviousViewedBooks": ["Example Book", "Another Example"],
+    "PreviousViewedArticles": ["Example Article", "Another Example"],})
     
     with open("User_Datastore.json", 'w') as uds:
         json.dump(UserData, uds , indent= 3)
@@ -59,4 +60,4 @@ def CreateNewID(UserData, UserID):
 
 
 # This tests the function in the terminal
-SaveData("romance", "006", "FavBookGenre")
+SaveData("Romance", "001", "FavBookGenre")
