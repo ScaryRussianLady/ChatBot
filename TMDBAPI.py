e19d4fd83bae964cea0c7d73200439df12c31d4c
jamdjam <warnock.jamie@yahoo.com>
2019-11-05 22:57:19 +0000

    movie search and and genre search now display readable information
    also went back to an earlier version of the genre search as it wasn't working properly

--- TMDBAPI.py
+++ TMDBAPI.py
@@ -15,6 +15,8 @@ def firstUserInt():
     FnctnFinder = input("please tell me what you would like to see so i can give it to you. ")
     FnctnFinderWords = FnctnFinder.split(" ")
 
+    #stores lists of words for possible user input
+    #code adapted from NewsAPI.py(created by Annija Balode)
     genList = ["genre", "genres"]
     searchList = ["search", "movie", "movies", "title"]
     upCList = ["upcoming", "future", "releases"]
@@ -39,7 +41,7 @@ def firstUserInt():
         elif FnctnFinderWords[x].lower() in topList:
             top_rated()
             break
-
+#end of block(Jamie)
 
 
 #--------------------------------------------------------MOVIE SEARCH FUNCTION--------------------------------------------------------#
@@ -61,13 +63,28 @@ def movie_search():
     #retreives information from the search and prints to the screen as somewhat readable text    
     response = req.get(url)
     movDict = response.json()
+
     #adapted code from stack overflow https://stackoverflow.com/questions/988228/convert-a-string-representation-of-a-dictionary-to-a-dictionary
-    strppdResults = str(movDict['results'])
-    resultsDict = ast.literal_eval(strppdResults.strip('[]'))
-    #end of adapted code fomr stack overflow
-    print('Title: '+ resultsDict['title'])
-    print(resultsDict['overview'])
-    print('With a release date of '+str(resultsDict['release_date'])+', this movie has mustered up a rating of '+ str(resultsDict['vote_average'])+'/10')
+    for title in movDict['results']:
+        titleDict = ast.literal_eval(str(title))
+        print('Title: '+ titleDict['title'])
+        print('Released on: '+ titleDict['release_date'])
+        print('Rated '+ str(titleDict['vote_average'])+'/10')
+        print('Overview: '+titleDict['overview'])
+        print('====================================')
+    #end of adapted code from stack overflow
+
+        #second attempt at getting code to work. introduced for loop(still only worked for one result)
+        #strppdResults = str(movDict['results'])
+        #resultsDict = ast.literal_eval(strppdResults.strip('[]'))
+        #for key in resultsDict:
+            #print(key+': '+ str(resultsDict[key]))
+        #print(title)
+
+    #before i introduced a for loop i used this (didn't work for multiple results)
+    #print('Title: '+ resultsDict['title'])
+    #print(resultsDict['overview'])
+    #print('With a release date of '+str(resultsDict['release_date'])+', this movie has mustered up a rating of '+ str(resultsDict['vote_average'])+'/10')
 
 #--------------------------------------------------------GENRE LIST SEARCH FUNCTION--------------------------------------------------------#
 
@@ -75,18 +92,11 @@ def movie_search():
 #reference https://developers.themoviedb.org/3/genres/get-movie-list
 def genre_list():
     print("=================================================================================================================================")
-    #used a dict to store possible search queries. more efficient than using a for and an if statement
-    #i know this is messy... I'm sorry
-    genreDict = {"action": "28", "adventure": "12", "animation": "16", "comedy": "35", "crime": "80", "documentary": "99", "drama": "18", "family": "10751", "fantasy": "14", "history": "36", "horror": "27", "music": "10402", "mystery": "9648", "romance": "10749", "science fiction": "878", "tv movie": "10770", "thriller": "53", "war": "10752", "western": "37"}
-    UserChoice = input("here is a list of genres available: 'Action', 'Adventure', 'Animation', 'Comedy', 'Crime', 'Documentary', 'Drama',\n'Family', 'Fantasy', 'History', 'Horror', 'Music', 'Mystery', 'Romance', 'Science Fiction', 'TV Movie', 'Thriller', 'War', and 'Western'.\n Please let me know which genre you would like to explore: ")
-    UserChoiceList = UserChoice.split(" ")
-    genreChoice = ""
-    #used to request the informaion from the servers
-    for c in range(len(UserChoiceList)):
-        for key in genreDict:
-            if str(c) in key:
-                genreChoice = genreDict[key]
-    url = "https://api.themoviedb.org/3/discover/movie?language=en-US&api_key="+api_key+"&with_genres="+str(genreChoice)
+    #used a dict to store possible search queries. more efficient than using an if statement
+    genreDict = {"Action": "28", "Adventure": "12", "Animation": "16", "Comedy": "35", "Crime": "80", "Documentary": "99", "Drama": "18", "Family": "10751", "Fantasy": "14", "History": "36", "Horror": "27", "Music": "10402", "Mystery": "9648", "Romance": "10749", "Science Fiction": "878", "TV Movie": "10770", "Thriller": "53", "war": "10752", "western": "37"}
+    UserChoice = input("here is a list of genres available: 'Action', 'Adventure', 'Animation', 'Comedy', 'Crime', 'Documentary', 'Drama', 'Family', 'Fantasy', 'History', 'Horror', 'Music', 'Mystery', 'Romance', 'Science Fiction', 'TV Movie', 'Thriller', 'War', and 'Western'. Please select one by typing it in: ")
+
+    url = "https://api.themoviedb.org/3/discover/movie?language=en-US&api_key="+api_key+"&with_genres="+str(genreDict[UserChoice])
 
 
     #retreives the genre data from server
@@ -94,9 +104,17 @@ def genre_list():
     #the "type" shows that it prtints a <dict>
     response = req.get(url)
     genDict = response.json()
-    print(genDict)
-    #print(type(genDict))
-    #print(list(genDict))
+
+    #adapted code from stack overflow https://stackoverflow.com/questions/988228/convert-a-string-representation-of-a-dictionary-to-a-dictionary
+    for title in genDict['results']:
+        titleDict = ast.literal_eval(str(title))
+        print('Title: '+ titleDict['title'])
+        print('Released on: '+ titleDict['release_date'])
+        print('Rated '+ str(titleDict['vote_average'])+'/10')
+        print('Overview: '+titleDict['overview'])
+        print('====================================')
+    #end of adapted code
+
     
 
 
@@ -109,13 +127,16 @@ def search_popular():
     choice = ""
     selection = input("Now, you can either see actors(and actresses) or movies. your choice. ")
     selectionWord = selection.split()
+    
+    selectionOne = ["actors", "actor", "actress", "actresses", "people"]
+    selectionTwo = ["films", "movies"]
 
     for i in range(len(selectionWord)):
 
-        if selectionWord[i].lower() == "actors" or selectionWord[i].lower() == "actor" or selectionWord[i].lower() == "actress" or selectionWord[i].lower() == "actresses" or selectionWord[i].lower() == "people":
+        if selectionWord[i].lower() in selectionOne:
             choice = "person"
             break
-        elif selectionWord[i].lower() == "films" or selectionWord[i].lower() == "movies":
+        elif selectionWord[i].lower() in selectionTwo:
             choice = "movie"
             break
 
