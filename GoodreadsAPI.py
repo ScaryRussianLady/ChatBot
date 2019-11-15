3617880ec870bda5f833a079e1f07f8566714f13
kazlone <1kazlone@gmail.com>
2019-11-15 15:13:21 +0000

    added comments

--- GoodreadsAPI.py
+++ GoodreadsAPI.py
@@ -28,7 +28,7 @@ from UserDataManagement import SaveData
 #End of code by [Annija Balode, ID No: 9102828] and referenced from https://rapidapi.com/raygorodskij/api/Goodreads.
 ######################################################################################################################################
 
-#[kofi]
+#[Gerald Owusu-Manu]
 #The Key:NxRD7Y051igsxA6xbjQRpQ
 #The secret: u65168BEZcYv39AJsYrc8hSjkLzA4boifPdsSEYbIw
 
@@ -36,60 +36,83 @@ from UserDataManagement import SaveData
 BookScriptGlobal_ID = "2151"
 
 
+#Start of block by [Gerald Owusu-Manu]
+
 #API can retrieve specific titles of books that the user requests.
-#[kofi]
+#reference https://www.goodreads.com/api/index#search.books
 def booksearch(MsgObj):
 
     # Next ONE line by Christian Shaw | Saves the ReplyID to the JSON database so that dialogue can be done on discord. Very important for user input.
     SaveData(BookScriptGlobal_ID+"_BookSearch2", MsgObj.userID, "ReplyID")
 
     #booktitle=input("Please type in the name of the book you would like to search")
+    #requests user to input a book name
     return "Please type in the name of the book you would like to search"
 
 def booksearch2(MsgObj):
     booktitle = MsgObj.msg
+    #The key is added to url to allow access to use the api and the name of the book inputted by the user is added
     url = ('https://www.goodreads.com/search.xml?key=NxRD7Y051igsxA6xbjQRpQ&q=' +booktitle)
+    #adapted code from https://www.youtube.com/watch?v=pKz1faPVNMA
+    # retreives the data from the url as text
     response = requests.get(url).text
     xml = BeautifulSoup(response, 'xml')
+
+    # previous adapted code no longer needed as modified by [Christian Shaw at the end of the script]
    #print ("Displaying results for  " +str(booktitle) + "... \n")
+    #prints only the title tag from the xml after 2 seconds
     #time.sleep(2)
+    #end of previous code
+
+
+    #xml function used to find the relevant tags that would output the information to the user
     Work = (xml.find("work"))
 
     # Next ONE line by Christian Shaw | Returns a natural reply using the dictionary data to the discord main script
     return NaturalReply(Work, "search", MsgObj)
 
     #for item in xml.findAll('title'):
+    #end of adapted code   
     #    print (item.text)
+# End of block [Gerald Owusu-Manu]
 
 
-#API can reviews of books that the user request by isbn.
-#"https://www.goodreads.com/book/isbn/"+isbn+"?key=NxRD7Y051igsxA6xbjQRpQ"
 
-#[kofi]        
+# Start of block [Gerald Owusu-Manu]
+# Gets reviews of a specific book from the given isbn
+# reference https://www.goodreads.com/book/isbn/ISBN?format=FORMAT     
+# How to pass info into url "https://www.goodreads.com/book/isbn/"+isbn+"?key=NxRD7Y051igsxA6xbjQRpQ"  
+
 def bookreview(MsgObj):
    
     # Next ONE line by Christian Shaw | Saves the ReplyID to the JSON database so that dialogue can be done on discord. Very important for user input.
     SaveData(BookScriptGlobal_ID+"_BookReview2", MsgObj.userID, "ReplyID")
 
     #isbn =input("Please type in the isbn of a book you would like to see the ratings of")
+    #askes the user to input isbn of the book they want ratings for
     return "Please type in the isbn of a book you would like to see the ratings of"
 
 def bookreview2(MsgObj):
 
     isbn = MsgObj.msg
-
+    # key is added to url along with isbn inputted by user
     url = ("https://www.goodreads.com/book/isbn/" +isbn+ "?key=NxRD7Y051igsxA6xbjQRpQ")
+    #adapted code from https://www.youtube.com/watch?v=pKz1faPVNMA
+    #result from the url is returned as text
     response = requests.get(url).text
     xml = BeautifulSoup(response, 'xml')
-
+    #xml function used to find the relevant tags that would output the information to the user
     Work = (xml.find("work"))
-
+    #end of adapted code
     # Next ONE line by Christian Shaw | Returns a natural reply using the dictionary data to the discord main script
     return NaturalReply(Work, "ratings", MsgObj)
 
-    #print ("Book reviews for isbn:" +str(isbn))
+    #previous adapted code no longer in use as modified by [Christian Shaw] at the end of the script
 
-   # print ("Book title:")
+    #organised with headings where the first result from each xml tag are displayed under
+    
+    #print ("Book reviews for isbn:" +str(isbn))
+    # print ("Book title:")
     #for item in xml.findAll('title')[0:1]:
     #    print (item.text)
     #print ("Author name:")
@@ -101,29 +124,40 @@ def bookreview2(MsgObj):
     #print ("Average Rating:")    
     #for item in xml.findAll('average_rating')[0:1]:
     #    print (item.text)
+    
+    #end of previous code
+    
+# End of block [Gerald Owusu-Manu]
 
-#[kofi]
+# Start of block [Gerald Owusu-Manu]
+
+#reference https://www.goodreads.com/api/index#search.books
+
 def releasedate(MsgObj):
 
     # Next ONE line by Christian Shaw | Saves the ReplyID to the JSON database so that dialogue can be done on discord. Very important for user input.
     SaveData(BookScriptGlobal_ID+"_ReleaseDate2", MsgObj.userID, "ReplyID")
 
     #booktitle=input("Please type in the name of the book you want the release date of")
+    #asks the user to input book name they want release date of
     return "Please type in the name of the book you want the release date of"
 
 def releasedate2(MsgObj):
-
+    #saves user input into booktitle variable
     booktitle = MsgObj.msg
-
+    #variable is passed into url
     url = ('https://www.goodreads.com/search.xml?key=NxRD7Y051igsxA6xbjQRpQ&q=' +booktitle)
+    #adapted code from https://www.youtube.com/watch?v=pKz1faPVNMA
+    #result from the url is returned as text
     response = requests.get(url).text
     xml = BeautifulSoup(response, 'xml')
-
+    #xml function used to find the relevant tags that would output the information to the user
     Work = (xml.find("work"))
+    #end of adapted code
 
     # Next ONE line by Christian Shaw | Returns a natural reply using the dictionary data to the discord main script
     return NaturalReply(Work, "date", MsgObj)
-    
+    #previous adapted code no longer needed as [modified by christian shaw at the end of the script]
     #print ("Year:")
     
     #for item in xml.findAll('original_publication_year')[0:1]:
@@ -132,31 +166,44 @@ def releasedate2(MsgObj):
     #       
     #for item in xml.findAll('original_publication_month')[0:1]:
     #    print (item.text)
-#[kofi]
+    # end of previous code
+# end of block [Gerald Owusu-Manu]
 
+#Start of block [Gerald Owusu-Manu]
 def authorMostpopular(MsgObj):
 
     # Next ONE line by Christian Shaw | Saves the ReplyID to the JSON database so that dialogue can be done on discord. Very important for user input.
     SaveData(BookScriptGlobal_ID+"_AuthorMostPopular2", MsgObj.userID, "ReplyID")
-
+    # asks user to input the name of the author
     #author=input("Please type in the name of the author you would like to search to see their most popular books")
     return "Please type in the name of the author you would like to search to see their most popular books"
 
 def authorMostpopular2(MsgObj):
+    #user input is saved into author variable
     author = MsgObj.msg
+    #author variable is passed into url to be searched
     url = ('https://www.goodreads.com/search.xml?key=NxRD7Y051igsxA6xbjQRpQ&q=' +author)
+    #adapted code from https://www.youtube.com/watch?v=pKz1faPVNMA
+    #result from the url is returned as text
     response = requests.get(url).text
     
+    # previous adapted code no longer needed as modified by [Christian Shaw at the end of the script]
     #print ("Displaying " +str(author) + " most popular books... \n")
+    #results are outputted after a 2 second delay
     #time.sleep(2)
+    #end of previous code
 
+
+    #xml function used to find the relevant tags that would output the information to the user
     xml = BeautifulSoup(response, 'xml')
 
     Work = (xml.find("work"))
-
+    #end of adapted code
     # Next ONE line by Christian Shaw | Returns a natural reply using the dictionary data to the discord main script
     return NaturalReply(Work, "search", MsgObj)
 
+    # previous adapted code no longer needed as modified by [Christian Shaw at the end of the script]
+    #headlines are created and displays the first name found in the author xml tag and outputs the most popular books found in the title tag
     #print ("Author name:")
     
     #for item in xml.findAll('name')[0:1]:
@@ -165,8 +212,11 @@ def authorMostpopular2(MsgObj):
     
     #for item in xml.findAll('title'):
     #    print (item.text)
+    #end of previous code
+  #End of block [Gerald Owusu-Manu]
 
-#[kofi]
+# Start of Block [Gerald Owusu-Manu]
+
 def UserIntro(MsgObj):
     
     # Next ONE line by Christian Shaw | Saves the ReplyID to the JSON database so that dialogue can be done on discord. Very important for user input.
@@ -175,15 +225,18 @@ def UserIntro(MsgObj):
     #print ("Hello, Welcome to the Book directory. \n I have the ability to provide you with book searches, book reviews/ratings and the release dates of any current or upcoming books and most popular books that an author has.")
     #UserOption  = input("What would you like to see?")
 
+    # displays the features the chatbot provides to the user and asks them what they want to see
     return "I have the ability to provide you with book searches, book reviews/ratings and the release dates of any current or upcoming books and most popular books that an author has.\nWhat would you like to see?"
 
 def UserIntro2(MsgObj):
     UserOptionWords = MsgObj.list
-    
+    #code adapted from NewsAPI.py [created by Annija Balode]
+    # stores a list of keywords that the user may enter 
     searchList = ["book","books","title",]
     releasedList = ["release", "releases", "date", "upcoming"]
     reviewList = ["rating", "rated","reviews","review"]
     PopList = ["popular", "most popular",]
+    #based on the keywords in the list if what the user enters corrosponds with a certain list a particular function is called
     for x in range(len(UserOptionWords)):
         if UserOptionWords[x].lower() in searchList:
             return booksearch(MsgObj)
@@ -197,7 +250,50 @@ def UserIntro2(MsgObj):
         elif UserOptionWords[x].lower() in PopList:
             return authorMostpopular(MsgObj)
             #break
+#end of adapted code
+    #end of block [ Gerald Owusu-Manu]
+# Start of block [Gerald Owusu-Manu]
+#Tried to implement a while loop in previous version of code which would allow the user to keep trying each time their input could not be found in the lists of keywords i made
+#def UserIntro():
+    #print ("Hello, Welcome to the Book directory. \n I have the ability to provide you with book searches, book reviews/ratings and the release dates of any current or upcoming books and most popular books that an author has.")
+    #UserOption  = input("What would you like to see?")
+    #UserOptionWords = UserOption.split(" ")
+    
+    #searchList = ["book","books","title",]
+    #releasedList = ["release", "releases", "date", "upcoming"]
+    #reviewList = ["rating", "rated","reviews","review"]
+    #PopList = ["popular", "most popular",]
+
+   #while UserOption not in (searchList,releasedList,reviewList,PopList):
+        #print ("option could not be found, please try again")
+        #UserOption  = input("What would you like to see?")
+
+    #if UserOption in (searchList,releasedList,reviewList,PopList):
+        #for x in range(len(UserOptionWords)):
+            #if UserOptionWords[x].lower() in searchList:
+                #booksearch()
+               #break
+            #elif UserOptionWords[x].lower() in releasedList:
+                #releasedate()
+                #break
+            #elif UserOptionWords[x].lower() in reviewList:
+                #bookreview()
+                #break
+            #elif UserOptionWords[x].lower() in PopList:
+                #authorMostpopular()
+                #break
+
 #UserIntro()
+#could not get it to function as even when the correct keywords were entered the loop would still iterate
+# End of block
+
+
+
+
+
+
+
+
 
 #--------------------------------------------------------REPLY SELECTION FUNCTION--------------------------------------------------------#
 
